ec215fd2093650842e9230e737211ecbd25bbf28
Preshak Bhattarai <117366207+PROX-GOD@users.noreply.github.com>
2024-04-12 18:28:01 +0545

    Upload units/test.jsx

---
+++ units/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SimplePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, React!</h1>
+        <p>This is a simple React webpage.</p>
+      </div>
+    );
+  }
+}
+
+export default SimplePage;
